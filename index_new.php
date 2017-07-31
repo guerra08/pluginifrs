b0ff775f645443f7403c4133e5a4ec5356c1fc4e
Bruno Gava Guerra <gguerrabruno@gmail.com>
2017-07-31 11:10:18 -0300

    Uploading files

---
+++ index_new.php
@@ -0,0 +1,180 @@
+<?php
+echo '<link rel="stylesheet" type="text/css" href="styles.css">';
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * sistec report
+ *
+ * @package    report
+ * @subpackage sistec
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require('../../config.php');
+require_once($CFG->dirroot.'/lib/tablelib.php');
+
+define('DEFAULT_PAGE_SIZE', 20);
+define('SHOW_ALL_PAGE_SIZE', 5000);
+
+$id         = required_param('id', PARAM_INT); // course id.
+$roleid     = optional_param('roleid', 0, PARAM_INT); // which role to show
+//$instanceid = optional_param('instanceid', 0, PARAM_INT); // instance we're looking at.
+$timefrom   = optional_param('timefrom', 0, PARAM_INT); // how far back to look...
+$action     = optional_param('action', '', PARAM_ALPHA);
+$page       = optional_param('page', 0, PARAM_INT);                     // which page to show
+$perpage    = optional_param('perpage', DEFAULT_PAGE_SIZE, PARAM_INT);  // how many per page
+$currentgroup = optional_param('group', 0, PARAM_INT); // Get the active group.
+
+$url = new moodle_url('/report/sistec/index.php', array('id'=>$id));
+if ($roleid !== 0) $url->param('roleid');
+//if ($instanceid !== 0) $url->param('instanceid');
+if ($timefrom !== 0) $url->param('timefrom');
+if ($action !== '') $url->param('action');
+if ($page !== 0) $url->param('page');
+if ($perpage !== DEFAULT_PAGE_SIZE) $url->param('perpage');
+$PAGE->set_url($url);
+$PAGE->set_pagelayout('admin');
+
+if ($action != 'view' and $action != 'post') {
+    $action = ''; // default to all (don't restrict)
+}
+
+if (!$course = $DB->get_record('course', array('id'=>$id))) {
+    print_error('invalidcourse');
+}
+
+if ($roleid != 0 and !$role = $DB->get_record('role', array('id'=>$roleid))) {
+    print_error('invalidrole');
+}
+
+require_login($course);
+$context = context_course::instance($course->id);
+require_capability('report/sistec:view', $context);
+
+$strsistec = get_string('sistecreport');
+$strviews         = get_string('views');
+$strposts         = get_string('posts');
+$strview          = get_string('view');
+$strpost          = get_string('post');
+$strallactions    = get_string('allactions');
+$strreports       = get_string('reports');
+
+$actionoptions = array('' => $strallactions,
+                       'view' => $strview,
+                       'post' => $strpost,);
+if (!array_key_exists($action, $actionoptions)) {
+    $action = '';
+}
+
+$PAGE->set_title($course->shortname .': '. $strsistec);
+$PAGE->set_heading($course->fullname);
+echo $OUTPUT->header();
+
+// Trigger a content view event.
+$event = \report_sistec\event\content_viewed::create(array('courseid' => $course->id,
+                                                               'other'    => array('content' => 'participants')));
+$event->set_page_detail();
+$event->set_legacy_logdata(array($course->id, "course", "report sistec",
+        "report/sistec/index.php?id=$course->id", $course->id));
+$event->trigger();
+
+$modinfo = get_fast_modinfo($course);
+
+$modules = $DB->get_records_select('modules', "visible = 1", null, 'name ASC');
+
+$instanceoptions = array();
+foreach ($modules as $module) {
+    if (empty($modinfo->instances[$module->name])) {
+        continue;
+    }
+    $instances = array();
+    foreach ($modinfo->instances[$module->name] as $cm) {
+        // Skip modules such as label which do not actually have links;
+        // this means there's nothing to participate in
+        if (!$cm->has_view()) {
+            continue;
+        }
+        $instances[$cm->id] = format_string($cm->name);
+    }
+    if (count($instances) == 0) {
+        continue;
+    }
+    $instanceoptions[] = array(get_string('modulenameplural', $module->name)=>$instances);
+}
+
+$timeoptions = array();
+// get minimum log time for this course
+$minlog = $DB->get_field_sql('SELECT min(time) FROM {log} WHERE course = ?', array($course->id));
+
+$now = usergetmidnight(time());
+
+// days
+for ($i = 1; $i < 7; $i++) {
+    if (strtotime('-'.$i.' days',$now) >= $minlog) {
+        $timeoptions[strtotime('-'.$i.' days',$now)] = get_string('numdays','moodle',$i);
+    }
+}
+// weeks
+for ($i = 1; $i < 10; $i++) {
+    if (strtotime('-'.$i.' weeks',$now) >= $minlog) {
+        $timeoptions[strtotime('-'.$i.' weeks',$now)] = get_string('numweeks','moodle',$i);
+    }
+}
+// months
+for ($i = 2; $i < 12; $i++) {
+    if (strtotime('-'.$i.' months',$now) >= $minlog) {
+        $timeoptions[strtotime('-'.$i.' months',$now)] = get_string('nummonths','moodle',$i);
+    }
+}
+// try a year
+if (strtotime('-1 year',$now) >= $minlog) {
+    $timeoptions[strtotime('-1 year',$now)] = get_string('lastyear');
+}
+
+// TODO: we need a new list of roles that are visible here
+$roles = get_roles_used_in_context($context);
+$guestrole = get_guest_role();
+$roles[$guestrole->id] = $guestrole;
+$roleoptions = role_fix_names($roles, $context, ROLENAME_ALIAS, true);
+
+// Writes HTML
+
+echo html_writer::div('<h1>Selecione uma funcionalidade:</h1>', 'plugin_select_text');
+
+echo html_writer::div('<img src="http://www.datamountain.com/wp-content/uploads/front_laptop.gif" height="100"><h4 class = "nomefuncao">SISTEC</h4>', 'plugin_select_sistec');
+
+echo html_writer::div('<img src="http://www.datamountain.com/wp-content/uploads/front_laptop.gif" alt="Evadidos"><h4 class = "nomefuncao">Evadidos</h4>' , 'plugin_select_evadidos');
+
+echo html_writer::div('<img src="http://www.datamountain.com/wp-content/uploads/front_laptop.gif" alt="CursosVazios"> <h4 class = "nomefuncao">Cursos<br>Vazios</h4>', 'plugin_select_cursosvazios');
+
+echo html_writer::div('<img src="http://www.datamountain.com/wp-content/uploads/front_laptop.gif" alt="Servidores"> <h4 class = "nomefuncao">Servidores</h4>', 'plugin_select_servidores');
+
+echo html_writer::div('<img src="http://www.datamountain.com/wp-content/uploads/front_laptop.gif" alt="Confirmar"> <h4 class = "nomefuncao">Confirmar<br>User</h4>', 'plugin_select_confirmar');
+
+//jQuery for linking
+
+/*echo '
+<script>
+    $( ".plugin_select_sistec" ).click(function () {
+        window.location = "https://moodle.ifrs.edu.br/report/sistec/index.php?id='.$id.'";
+    });
+</script>';*/
+
+$PAGE->requires->js_init_call('M.report_sistec.init');
+
+echo $OUTPUT->footer();
